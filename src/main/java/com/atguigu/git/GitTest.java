7c6a303403a4855abc3253eb5ecfbd019cac21ee
xiaoxin <xiaoxin@qq.com>
2022-10-06 20:08:49 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,16 @@
+package com.atguigu.git;
+
+/**
+ * @author long
+ * @create 2022 10 06 20:05
+ */
+public class GitTest {
+
+        public static void main(String[] args) {
+            System.out.println("Hello git!");
+
+
+        }
+    }
+
+
